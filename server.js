3de8375a041ea701cd1270184458a11c16bb5e48
PnGx27 <lilpeng69@gmail.com>
2026-05-26 19:30:37 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const http = require('http');
+const Gun = require('gun');
+
 const app = express();
-const server = require('http').createServer(app);
-require('gun')({ web: server });
-server.listen(process.env.PORT || 3000, () => console.log('relay online'));
+const server = http.createServer(app);
+
+app.use(Gun.serve);
+Gun({ web: server });
+
+app.get('/', (req, res) => res.send('GarlicChat relay online ✓'));
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log('GarlicChat relay online');
+});
